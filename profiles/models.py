6b5d2a9c04a7e18550460b43f27d1350f80dcfc5
Sacvartello <georgdvorskii@gmail.com>
2025-10-09 13:38:17 +0300

    Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    PROFESSION_CHOICES = [('ST','Student'),('TC','Teacher')]
+
+    role = models.CharField(
+        max_length=120,
+        choices=PROFESSION_CHOICES
+    )
+    class_field = models.IntegerField()
+    date_of_birth = models.DateField()
+    bio = models.TextField()
+    hobbies = models.TextField(
+        blank=True,
+        null=True
+    )
+    interesting_facts = models.CharField(
+        max_length=300,
+        blank=True,
+        null=True
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
