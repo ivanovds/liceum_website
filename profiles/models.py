920d69ae48d0d45f1035935cf0d565ed9f4401cd
Kyryl <okhrimenkokyryl@gmail.com>
2025-10-09 13:22:04 +0300

    Added profiles database

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="user")
+    role = models.TextChoices("student", "teacher")
+    class_ = models.TextField(max_length=20)
+    date_of_birth = models.DateField()
+    bio = models.TextField(max_length=500)
+    hobbies = models.TextField(max_length=100)
+    interesting_facts = models.TextField(max_lenght=500)
