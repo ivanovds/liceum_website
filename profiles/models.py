8333f0a8603e9a092349336c265faacb95bfdc35
Kyryl <okhrimenkokyryl@gmail.com>
2025-10-09 13:25:37 +0300

    Fixed profiles database

--- profiles/models.py
+++ profiles/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="user")
     role = models.TextChoices("student", "teacher")
-    class_ = models.TextField(max_length=20)
+    grade = models.TextField(max_length=20)
     date_of_birth = models.DateField()
-    bio = models.TextField(max_length=500)
-    hobbies = models.TextField(max_length=100)
-    interesting_facts = models.TextField(max_lenght=500)
+    bio = models.TextField()
+    hobbies = models.TextField()
+    interesting_facts = models.TextField()
