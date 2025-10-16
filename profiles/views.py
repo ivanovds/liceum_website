96c6b1b9467371bbfcdde5a5193beae3b4a826c1
Sacvartello <georgdvorskii@gmail.com>
2025-10-16 13:40:24 +0300

    Add login and logout views
    # Conflicts:
    #	profiles/models.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def login_view(request):
+    print(request)
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"Вітаємо, {user.username}!")
+            return redirect(request.GET.get('next', 'home'))
+        else:
+            messages.error(request, "Невірне ім’я користувача або пароль.")
+            return render(request, "login.html")
+
+    return render(request, "login.html")
+
+@login_required()
+def logout_view(request):
+    logout(request)
+    return redirect('/')
