abfd65985194556c411530a904be76fe96843f5b
ivanovds <work.ivanovds@gmail.com>
2025-10-23 00:59:20 +0300

    Fixed views and forms

--- profiles/forms.py
+++ profiles/forms.py
@@ -4,15 +4,28 @@ from django import forms
 
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     email = forms.CharField(max_length=150, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
 
     class Meta:
       model = User
-      fields = ('email', 'password1', 'password2')
+      fields = ('username',  'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('A user with this email already exists.')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError('A user with this username already exists.')
+        return username
 
 
 class LoginForm(forms.Form):
-    email = forms.CharField(max_length=150, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
--- profiles/views.py
+++ profiles/views.py
@@ -2,10 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
-from django.shortcuts import render
 from django.views import View
-from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 
 from profiles.forms import UserRegisterForm, LoginForm
@@ -14,41 +11,33 @@ from profiles.forms import UserRegisterForm, LoginForm
 class RegisterView(View):
     def __init__(self):
         super().__init__()
-        self.__template_name = 'profiles/register.html'
+        self.__template_name = 'register.html'
 
     def get(self, request, *args, **kwargs):
-        form = UserRegisterForm()
-        return render(request, self.__template_name, {'form': form})
-
-    @classmethod
-    def validate_user(cls, username, password):
-        if User.objects.filter(username=username, password=password).exists():
-            raise ValueError('Hehe, you can`t create several same users')
-#blahblahblah@gmail.com
-    @classmethod
-    def save_user(cls, form, username, password):
-        extra_fields = form.cleaned_data.pop('password1').pop('password2').pop('username')
-        User.objects.create_user(username, password=password, **extra_fields)
+        sign_up_form = UserRegisterForm()
+        return render(request, self.__template_name, {'sign_up_form': sign_up_form})
 
     def post(self, request, *args, **kwargs):
         if request.POST is None:
             raise ValueError('No POST data got!')
 
-        form = UserRegisterForm(request.POST)
+        sign_up_form = UserRegisterForm(request.POST)
 
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
+        if sign_up_form.is_valid():
+            email = sign_up_form.cleaned_data.get('email')
+            username = sign_up_form.cleaned_data.get('username')
+            password = sign_up_form.cleaned_data.get('password1')
 
-            self.validate_user(username, password)
-            self.save_user(form, username, password)
+            user = User.objects.create_user(username=username, email=email, password=password)
 
-        return HttpResponseRedirect('redirect url')
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, self.__template_name, {'sign_up_form': sign_up_form})
 
 
 def login_view(request):
-    print(request)
-    form = LoginForm()
+    login_form = LoginForm()
     if request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
@@ -57,13 +46,11 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            messages.success(request, f"Вітаємо, {user.username}!")
-            return redirect(request.GET.get('next', 'home'))
+            return redirect('/')
         else:
-            messages.error(request, "Невірне ім’я користувача або пароль.")
-            return render(request, "profiles/login.html", {'form': form})
+            messages.error(request, "User not found")
 
-    return render(request, "profiles/login.html", {'form': form})
+    return render(request, "login.html", {'login_form': login_form})
 
 
 @login_required()
