fd769572167019e00e719176d456c5d24f485d7e
ivanovds <work.ivanovds@gmail.com>
2025-12-18 13:06:41 +0200

    Added posts application

--- liceum_website/urls.py
+++ liceum_website/urls.py
@@ -23,4 +23,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('profiles.urls')),
+    path('posts/', include('posts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
