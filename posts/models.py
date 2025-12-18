33dd74b10d8a680d6e3f428c698e1ba82f8fa93f
jedig <ateist29a@gmail.com>
2025-12-18 12:30:18 +0100

    Added posts

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+def upload_image(instance, filename):
+    """Returns path to image in media directory."""
+    return '/'.join(['posts', str(instance.id), filename])
 
 
 class Post(models.Model):
-    pass
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="user")
+    main_text = models.CharField(max_length=500, blank=True, null=True)
+    image = models.ImageField(upload_to=upload_image, null=False, blank=False, default='default_avatar.jpg', )
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
